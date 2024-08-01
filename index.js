ae1abc77bd9f93a4692ba408f08d89a687d00279
showmyaE <showmya3@gmail.com>
2024-08-01 15:46:54 +0530

    add

--- index.js
+++ index.js
@@ -39,3 +39,29 @@ app.get('/bookinglist',async(req,res)=>{
     res.send(userDbDetails)
 
 })
+// id: 2,
+// name: "Jerrsy, United State",
+// Villas: "12 Villas",
+// Apartments: "10 Apartments",
+// Offices: "07 Offices",
+// cover: "./images/location/city-2.png",
+app.get('/location',async(req,res)=>{
+//    const createTable=`CREATE TABLE LocationTable(
+//         id int,
+//         name varchar(255),
+//         Villas varchar(255),
+//         Appartments varchar(255),
+//         Offices varchar(255),
+//         cover varchar(255)
+//     );`
+//     const createData=await db.run(createTable);
+    // const insertquery=`INSERT INTO LocationTable(id,name,Villas,Appartments,Offices,cover)
+    // VALUES (6,'California, USA', '12 Villas',"10 Apartments","07 Offices", "./images/location/city-6.png")`;
+    // const insertData=await db.run(insertquery)
+    const getUserQuery=`select * from LocationTable`;
+
+    const userDbDetails=await db.all(getUserQuery);
+    console.log('DB value',userDbDetails);
+    res.send(userDbDetails)
+
+})
